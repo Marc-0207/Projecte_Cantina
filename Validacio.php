1ed29341ff9ca3d685e6eb52da76b0d74b792a55
jost243 <92725911+jost243@users.noreply.github.com>
2021-10-21 08:08:38 +0200

    Update Validacio.php

--- Validacio.php
+++ Validacio.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 include 'header.php';
 echo "<h1><u>Vols compara aquests productes?</h1></u>";
@@ -50,3 +51,52 @@ foreach($_POST as $pro => $value) {
 <?php
 include 'footer.php';
 ?>
+=======
+<?php
+include 'header.php';
+echo "<h1><u>Vols compara aquests productes?</h1></u>";
+//echo $_GET["prodcuto"];
+?>
+<p>Aquesta es la validacio</p>
+
+
+
+<div id="contenidor_val">
+    <div id="validacio">
+        <table>
+            <tr>
+                <td><label>Nom: </label></td>
+                <td><input type="text" name="nom"></td>
+            </tr>
+            <tr>
+                <td><label>Telefon: </label></td>
+                <td><input type="text" name="telefon"></td>
+            </tr>
+            <tr>
+                <td><label>Correu electronic: </label></td>
+                <td><input type="text" name="correu"></td>
+            </tr>
+        </table>
+    </div>
+
+        <div id= "compra">
+            <h1>Hola</h1>
+
+            <br>
+            <input type="submit" value="compras"/>
+        </div>
+</div>
+
+
+<div>
+    <form action="./Finalitzar.php">
+        <input type="submit" value="Següent"/>
+    </form>
+    <div>
+        <a href="./Menu.php"><button>Tornar</button></a>
+    </div>
+</div>
+<?php
+include 'footer.php';
+?>
+>>>>>>> Stashed changes
