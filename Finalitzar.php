a4ea35430db781848285039665a351b6c88b421c
a18kevhoylor <a18kevhoylor@inspedralbes.cat>
2021-10-21 10:46:36 +0200

    menu tarda/mati

--- Finalitzar.php
+++ Finalitzar.php
@@ -1,20 +1,7 @@
 <?php
 include 'header.php';
 ?>
-<h1><u><br>Tiquet de compra</u></h1>
-<?php
-echo "Nom: ". $_GET["nom"]. "<br>";
-echo "Correu: ". $_GET["correu"]. "<br>";
-echo "Telefon: ". $_GET["telefon"]. "<br>";
-
-foreach($_POST as $pro => $value) {
-
-    if ($value!=0) {
-        print "$pro : $value";
-    }
-}
-?>
-
+<p>Aquesta es la pantalla de finalització</p>
 <form action="./Finalitzar.php">
     <input type="submit" value="Acabar"/>
 </form>
--- Menu.php
+++ Menu.php
@@ -19,7 +19,7 @@ include 'header.php';
 
         <?php
         echo "mati";
-        $menu_json = file_get_contents('menu.json');
+        $menu_json = file_get_contents('menuMati.json');
         $decoded_json = json_decode($menu_json, true);
         foreach($decoded_json as $key => $value) {
             $pro = $decoded_json[$key]["producto"];
@@ -28,7 +28,7 @@ include 'header.php';
             echo "<div id='$id'>
                     <h2> $pro </h2> $pre <br>
                     <input type='button' value='+' class='afegir'>
-                    <input type='text' id=i$id name =$pro value='0'> 
+                    <input type='text' id=im_$id name =$pro value='0'> 
                     <input type='button' value='-' class='treure'>
                     </div>";
         }
@@ -36,24 +36,24 @@ include 'header.php';
     
         <input type="submit" value="Comprar">
 
-
-   <!-- Menu Tarda-->
     </form>
+    <!-- Menu Tarda-->
 
         <form method="POST" action="Validacio.php" id="tarda">
 
         <?php
         echo "tarda";
-        $menu_json = file_get_contents('menu.json');
+        $menu_json = file_get_contents('menuTarda.json');
         $decoded_json = json_decode($menu_json, true);
         foreach($decoded_json as $key => $value) {
             $pro = $decoded_json[$key]["producto"];
             $pre = $decoded_json[$key]["precio"];
             $id = $decoded_json[$key]["id"];
+            
             echo "<div id='$id'>
                     <h2> $pro </h2> $pre <br>
                     <input type='button' value='+' class='afegir'>
-                    <input type='text' id=i$id name =$pro value='0'> 
+                    <input type='text' id=it_$id name =$pro value='0'> 
                     <input type='button' value='-' class='treure'>
                     </div>";
         }
@@ -72,31 +72,40 @@ include 'header.php';
        let mati=document.getElementById("mati");
 
        let mati_o_tarda="";
-       if (hora >= 14){
+       if (hora <= 14){
             mati_o_tarda="tarda";
-           mati.style.display = "none";
+            mati.style.display = "none";
        }
        else {
-           mati_o_tarda="mati";
+            mati_o_tarda="mati";
             tarda.style.display = "none";
         }
+      
 
     document.getElementById(mati_o_tarda).addEventListener('click', e => {
+
 
         if(e.target.classList.contains('afegir')){
             afegirProducte(e.target.parentNode.id);
             console.log(e.target.parentNode.id);
         }
-        else if (e.target.classList.contains('treure')){
+        else if (e.target.classList.contains('treure')){            
             treureProducte(e.target.parentNode.id);
         }
         function afegirProducte(idProducte){
-            document.getElementById("i"+idProducte).value++;
+        
+            document.getElementById((mati_o_tarda=="mati" ? "im_" : "it_") + idProducte).value++;
+    
         }
-        function treureProducte(idProducte){
-            if(document.getElementById("i"+idProducte).value>0){
-                document.getElementById("i"+idProducte).value--;
+
+        function treureProducte(idParcial){
+
+            let idProducto = (mati_o_tarda=="mati" ? "im_" : "it_") + idParcial;
+
+            if(document.getElementById(idProducto).value>0){
+                document.getElementById(idProducto).value--;
             }}
+
     });
 
 </script>
