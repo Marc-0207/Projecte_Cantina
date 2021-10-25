04f82293296920aca26dca8583e67c35ffba7858
jost243 <a20joslomdur@inspedralbes.cat>
2021-10-26 00:21:38 +0200

    Update Validacio.php

--- Validacio.php
+++ Validacio.php
@@ -12,65 +12,70 @@ session_start();
 <body>
 
 
+    <?php
+    include 'header.php';
+    ?>
 
-<?php
-include 'header.php';
-$menu = file_get_contents('menuTarda.json');
+        <form method="GET" action="Finalitzar.php">
+            <div id="contenidor_val">
+                <div id="validacio">
+                    <table>
+                        <tr>
+                            <td><label>Nom: &emsp;&emsp;&emsp;</label></td>
+                            <td><input type="text" name="nom" required style="margin-bottom: 5px" size="40"></td>
+                        </tr>
+                        <tr>
+                            <td><label>Telefon: </label></td>
+                            <td><input type="text" name="telefon" id="telefon" ></td>
+                        </tr>
+                        <tr>
+                            <td><label>Correu electronic: </label></td>
+                            <td><input type="text" name="correu"></td>
+                        </tr>
+                    </table>
+                </div>
 
-$menu_json = json_decode($menu,true);
+                <div id= "compra">
+                    <div>
+                        <?php
+                            $menu = file_get_contents('menuTarda.json');
 
-$Preu_total = 0;
-foreach ($_POST as $id => $value) {
-    if ($value != 0) {
-        echo "Nombre de producto: " . $menu_json[$id]["producto"] .
-            "<br>" .
-            "Unitats: " . $value .
-            "<br>" .
-            "Preu unitari: " . $menu_json[$id]["precio"] . "€" .
-            "<br>" .
-            "Preu total: " . $menu_json[$id]["precio"] * $value . "€" .
-            "<br><br>";
-        $Preu_total += $menu_json[$id]["precio"] * $value;
-        $_SESSION["Nombre_pro"]=$menu_json[$id]["producto"];
-    }
-}
-"<br><br>";
-echo "Preu Total de los productos: ".$Preu_total."€";
-?>
+                            $menu_json = json_decode($menu, true);
 
-<form method="GET" action="Finalitzar.php">
-    <div id="contenidor_val">
-        <div id="validacio">
-            <table>
-                <tr>
-                    <td><label>Nom: &emsp;&emsp;&emsp;</label></td>
-                    <td><input type="text" name="nom" required style="margin-bottom: 5px" size="40"></td>
-                </tr>
-                <tr>
-                    <td><label>Telefon: </label></td>
-                    <td><input type="text" name="telefon" id="telefon" ></td>
-                </tr>
-                <tr>
-                    <td><label>Correu electronic: </label></td>
-                    <td><input type="text" name="correu"></td>
-                </tr>
-            </table>
-        </div>
+                            $Preu_total = 0;
+                            foreach ($_POST as $id => $value) {
+                                if ($value != 0) {
+                                    echo "Nombre de producto: " . $menu_json[$id]["producto"] .
+                                        "<br>" .
+                                        "Unitats: " . $value .
+                                        "<br>" .
+                                        "Preu unitari: " . $menu_json[$id]["precio"] . "€" .
+                                        "<br>" .
+                                        "Preu total: " . $menu_json[$id]["precio"] * $value . "€" .
+                                        "<br><br>";
+                                    $Preu_total += $menu_json[$id]["precio"] * $value;
+                                    $_SESSION["Nombre_pro"] = $menu_json[$id]["producto"];
+                                }
+                            }
+                            "<br><br>";
+                            echo "Preu Total de los productos: " . $Preu_total . "€";
 
-        <div id= "compra">
-            <h1>Hola</h1>
-            <div>
-                <form action="./Finalitzar.php">
-                    <input type="submit" value="Següent"/>
-                </form>
-                <div>
-                    <a href="./Menu.php"><button>Tornar</button></a>
+                        ?>
+                        <div id="finalitzar">
+                            <form action="./Finalitzar.php">
+                                <input type="submit" value="Següent"/>
+                            </form>
+                            <a href="./Menu.php"><button>Tornar</button></a>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </form>
+
+            <script type="text/javascript" src="Validacio.js"></script>
 
             <?php
             include 'footer.php';
             ?>
-            <script type="text/javascript" src="Validacio.js"></script>
 </body>
 </html>
