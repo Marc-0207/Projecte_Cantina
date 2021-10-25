93ec32947fb3b30da6601523cafcf36c87dd863a
a18kevhoylor <a18kevhoylor@inspedralbes.cat>
2021-10-25 14:21:38 +0200

    CSS index

--- index.php
+++ index.php
@@ -1,14 +1,56 @@
-<?php
-include 'header.php';
-?>
-<p>Aquesta es la pantalla principal</p>
-<div>
-    <a href="./Menu.php"><button>Anar al Menu</button></a>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="ClasesCSS.css">
+    <link rel="stylesheet" href="normalize.css">
+    <?php
+    include 'header.php';
+    ?>
+
+</head>
+<body>
+<div id="menuIndex">
 </div>
-<div>
-    <a href="./Administracio.php"><button>Administracio</button></a>
+<div id="prinIndex">
+
+    <div id="latdIndex">
+        <div class="imgLat">
+        
+        </div>
+        <div class="imgLat">
+        
+        </div>
+        <div class="imgLat">
+
+        </div>
+      
+    </div>
+    <div id="cenIndex">
+        
+        
+    </div>
+
+    <div id="latiIndex">
+        <div class="imgLat">
+            
+        </div>
+        <div class="imgLat">
+        
+        </div>
+        <div class="imgLat">
+
+        </div>
+
+    </div>
+
+
 </div>
+
 <?php
 include 'footer.php';
 ?>
+</body>
+</html>
 
