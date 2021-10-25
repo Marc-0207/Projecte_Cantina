0dcda6b990c8cbb320c0e45ae38236b9ae6733b0
a18marcastru <a18marcastru@inspedralbes.cat>
2021-10-25 08:36:58 +0200

    menuActualizado

--- Menu.php
+++ Menu.php
@@ -15,90 +15,94 @@ include 'header.php';
 <!-- Menu Mati-->
 
 
-    <form method="POST" action="Validacio.php" id="mati">
+<form method="POST" action="Validacio.php" id="mati">
 
-        <?php
-        echo "mati";
-        $menu_json = file_get_contents('menuMati.json');
-        $decoded_json = json_decode($menu_json, true);
-        foreach($decoded_json as $key => $value) {
-            $pro = $decoded_json[$key]["producto"];
-            $pre = $decoded_json[$key]["precio"];
-            $id = $decoded_json[$key]["id"];
-            echo "<div id='$id'>
+    <?php
+    echo "mati";
+    $menu_json = file_get_contents('menuMati.json');
+    $decoded_json = json_decode($menu_json, true);
+    foreach($decoded_json as $key => $value) {
+        $pro = $decoded_json[$key]["producto"];
+        $pre = $decoded_json[$key]["precio"];
+        $id = $decoded_json[$key]["id"];
+        echo "<div id='$id'>
                     <h2> $pro </h2> $pre <br>
                     <input type='button' value='+' class='afegir'>
-                    <input type='text' id=im_$id name =$id value='0'> 
+                    <input type='text' id=im_$id name =$pro class='cantidades' value='0'> 
                     <input type='button' value='-' class='treure'>
                     </div>";
-        }
-        echo "<input id ='json' type='hidden' value='0'/>";
-        echo "<input id ='json' type='hidden' value='$menu_json'/>";
-        ?>
-        <h2><u>Total productes</u></h2><br>
-        <input type="submit" value="Comprar"/>
+    }
+    echo "<input id ='json' type='hidden' value='$menu_json'>";
+    ?>
 
-    </form>
-    <!-- Menu Tarda-->
+    <div id="ticket">
 
-        <form method="POST" action="Validacio.php" id="tarda">
+    </div>
+    <input type="submit" value="Comprar">
 
-        <?php
-        echo "tarda";
-        $menu_json = file_get_contents('menuTarda.json');
-        $decoded_json = json_decode($menu_json, true);
-        foreach($decoded_json as $key => $value) {
-            $pro = $decoded_json[$key]["producto"];
-            $pre = $decoded_json[$key]["precio"];
-            $id = $decoded_json[$key]["id"];
-            
-            echo "<div id='$id'>
+</form>
+<!-- Menu Tarda-->
+
+<form method="POST" action="Validacio.php" id="tarda">
+
+    <?php
+    echo "tarda";
+    $menu_json = file_get_contents('menuTarda.json');
+    $decoded_json = json_decode($menu_json, true);
+    foreach($decoded_json as $key => $value) {
+        $pro = $decoded_json[$key]["producto"];
+        $pre = $decoded_json[$key]["precio"];
+        $id = $decoded_json[$key]["id"];
+
+        echo "<div id='$id'>
                     <h2> $pro </h2> $pre <br>
                     <input type='button' value='+' class='afegir'>
-                    <input type='text' id=it_$id name =$pro value='0'> 
+                    <input type='text' id=it_$id name =$pro class='cantidades' value='0'> 
                     <input type='button' value='-' class='treure'>
                     </div>";
-        }
-        echo "<input name='ident' type='hidden' value='1'>";
-        echo "<input id ='json' type='hidden' value='$menu_json'>";
-        ?>
-        <input type="submit" value="Comprar">
-    </form>
+    }
+    echo "<input id ='json' type='hidden' value='$menu_json'>";
+    ?>
+
+    <div id="ticket">
+
+    </div>
+    <input type="submit" value="Comprar">
+</form>
 <script>
 
     const dia = new Date();
-       let hora = dia.getHours();
-       
-       console.log(hora);
+    let hora = dia.getHours();
 
-       let tarda=document.getElementById("tarda");
-       let mati=document.getElementById("mati");
+    console.log(hora);
 
-       let mati_o_tarda="";
-       if (hora >= 14){
-            mati_o_tarda="tarda";
-            mati.style.display = "none";
-       }
-       else {
-            mati_o_tarda="mati";
-            tarda.style.display = "none";
-        }
-      
+    let tarda=document.getElementById("tarda");
+    let mati=document.getElementById("mati");
+
+    let mati_o_tarda="";
+    if (hora >= 14){
+        mati_o_tarda="tarda";
+        mati.style.display = "none";
+    }
+    else {
+        mati_o_tarda="mati";
+        tarda.style.display = "none";
+    }
+
 
     document.getElementById(mati_o_tarda).addEventListener('click', e => {
 
         if(e.target.classList.contains('afegir')){
             afegirProducte(e.target.parentNode.id);
-            console.log(e.target.parentNode.id);
-            ticket(e.target.parentNode.id);
+            //console.log(e.target.parentNode.id);
         }
-        else if (e.target.classList.contains('treure')){            
+        else if (e.target.classList.contains('treure')){
             treureProducte(e.target.parentNode.id);
         }
         function afegirProducte(idProducte){
 
             document.getElementById((mati_o_tarda=="mati" ? "im_" : "it_") + idProducte).value++;
-
+            actualitzarTicket(menuList);
         }
 
         function treureProducte(idParcial){
@@ -107,16 +111,38 @@ include 'header.php';
 
             if(document.getElementById(idProducto).value>0){
                 document.getElementById(idProducto).value--;
+                actualitzarTicket(menuList);
             }
         }
 
     });
-    function ticket(nomProducte, quantitat, idProducte){
-        nomProducte = ((mati_o_tarda=="mati" ? "im_" : "it_") + idProducte).name
-        quantitat = ((mati_o_tarda=="mati" ? "im_" : "it_") + idProducte).value
-        console.log(nomProducte, quantitat);
+    function actualitzarTicket(datosMenu){
 
+        let ticket=document.getElementById("ticket");
+
+        cantidades = document.getElementsByClassName("cantidades");
+        let textTicket="";
+        let Preu_total=0;
+        for(let index = 0;index < cantidades.length;index++){
+            console.log(cantidades.length);
+            if(cantidades[index].value!=0){
+                textTicket += " Article: " + datosMenu[cantidades[index-1].parentNode.id].producto;
+                textTicket += "<br>";
+                textTicket += " Unitats: " + cantidades[index].value;
+                textTicket += "<br>";
+                textTicket +="   Preu unitari: " + datosMenu[cantidades[index-1].parentNode.id].precio +"€";
+                textTicket += "<br>";
+                textTicket +="   Preu total:   " + datosMenu[cantidades[index-1].parentNode.id].precio * cantidades[index].value +"€";
+                Preu_total +=  datosMenu[cantidades[index].parentNode.id].precio * cantidades[index].value;
+                textTicket += "<br><br>";
+            }
+        }
+        "<br><br>";
+        textTicket+="<h2>   Preu total de todos los productos:   " +  Preu_total + "€</h2>";
+
+        ticket.innerHTML = textTicket;
     }
+    menuList = JSON.parse(document.getElementById("json").value);
 
 </script>
 
