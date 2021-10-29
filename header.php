1cfe1e8aac0f4fbe31a4e91e5f83c610dad56998
jost243 <a20joslomdur@inspedralbes.cat>
2021-10-29 23:22:33 +0200

    Update header.php

--- header.php
+++ header.php
@@ -2,6 +2,11 @@
     <div id= "header">
         <img id="logimg" src="/img/logo.png">
         <h1>Cantina Pedralbes</h1>
+        <div id="botones">
+            <a href="./index.php"><button class="third">Index</button></a>
+            <a href="./Menu.php"><button class="third">Menu</button></a>
+            <a href="./admin/Administracio.php"><button class="third">Administracio</button></a>
+        </div>
     </div>
 
 </body>
